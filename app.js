14d5170df1c4e5e80458f85ff8405259a5597a7c
RGNCertus <74768218@certus.edu.pe>
2024-09-24 09:33:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ let indefinido;
 let undef= undefined;
 let nulo= null;
 
+let precios = [429.99, 229.99, 59.00]; // Precios de los productos
+        let suma = precios.reduce((total, precio) => total + precio, 0);
+        document.getElementById("sumaPrecios").textContent = 'S/.' + suma.toFixed(2);
+
 
